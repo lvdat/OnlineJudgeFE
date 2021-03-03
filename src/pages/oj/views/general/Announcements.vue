838841915837fcc8a5046de1b6a52c41c3332e42
lvdat <datb2012191@student.ctu.edu.vn>
2021-03-03 13:56:48 +0700

    small update ui

--- src/pages/oj/views/general/Announcements.vue
+++ src/pages/oj/views/general/Announcements.vue
@@ -1,68 +1,79 @@
 <template>
-  <Panel shadow :padding="10">
-    <div slot="title">
-      {{title}}
-    </div>
-    <div slot="extra">
-      <Button v-if="listVisible" type="info" @click="init" :loading="btnLoading">{{$t('m.Refresh')}}</Button>
-      <Button v-else type="ghost" icon="ios-undo" @click="goBack">{{$t('m.Back')}}</Button>
-    </div>
+  <div class="flex-container">
+    <div id="main">
+      <Panel shadow :padding="10" >
+        <div slot="title">
+          {{title}}
+        </div>
+        <div slot="extra">
+          <Button v-if="listVisible" type="info" @click="init" :loading="btnLoading"><span class="ivu-icon ivu-icon-refresh"></span> {{$t('m.Refresh')}}</Button>
+          <Button v-else type="ghost" icon="ios-undo" @click="goBack">{{$t('m.Back')}}</Button>
+        </div>
+        <transition-group name="announcement-animate" mode="in-out">
+          <div class="no-announcement" v-if="!announcements.length" key="no-announcement">
+            <p>{{$t('m.No_Announcements')}}</p>
+          </div>
+          <template v-if="listVisible">
+            <ul class="announcements-container" key="list">
+              <li v-for="announcement in announcements" :key="announcement.title">
+                <div class="flex-container">
+                  <div class="title"><a class="entry" @click="goAnnouncement(announcement)">
+                    {{announcement.title}}</a></div>
+                  <div class="date">{{announcement.create_time | localtime }}</div>
+                  <div class="creator"> {{$t('m.By')}} {{announcement.created_by.username}}</div>
+                </div>
+              </li>
+            </ul>
+            <Pagination v-if="!isContest"
+                        key="page"
+                        :total="total"
+                        :page-size="limit"
+                        @on-change="getAnnouncementList">
+            </Pagination>
+          </template>
 
-    <transition-group name="announcement-animate" mode="in-out">
-      <div class="no-announcement" v-if="!announcements.length" key="no-announcement">
-        <p>{{$t('m.No_Announcements')}}</p>
-      </div>
-      <template v-if="listVisible">
-        <ul class="announcements-container" key="list">
-          <li v-for="announcement in announcements" :key="announcement.title">
-            <div class="flex-container">
-              <div class="title"><a class="entry" @click="goAnnouncement(announcement)">
-                {{announcement.title}}</a></div>
-              <div class="date">{{announcement.create_time | localtime }}</div>
-              <div class="creator"> {{$t('m.By')}} {{announcement.created_by.username}}</div>
+          <template v-else>
+          <div v-katex v-html="announcement.content" key="content" class="content-container markdown-body"></div>
+          </template>
+        </transition-group>
+      </Panel>
+      <Row v-if="!isContest" type="flex" :gutter="10" style="margin-top: 70px;">
+        <Col :span="12">
+            <Panel shadow style="padding-top: 0px;padding-bottom: 10px;">
+            <div slot="title" style="margin-left: -10px;margin-bottom: -10px;">Bài tập mới</div>
+            <ul style="margin-left: 40px;margin-bottom: 20px;">
+                <li style="padding: 5px 0px;"  v-for="p in problemList" :key="p.id">
+                <a class="link-style" :href="'/problem/' + p._id">{{p._id}} - {{p.title}}</a>
+                </li>
+            </ul>
+            </Panel>
+        </Col>
+        <Col :span="12">
+          <Panel shadow style="padding-top: 0px;padding-bottom: 10px;">
+            <div slot="title" style="margin-left: -10px;margin-bottom: -10px;">Về CodeTrain</div>
+            <div class="content-container markdown-body">
+              <b style="color:red">Code</b><b style="color:yellow">Train</b> chính thức đi vào hoạt động công khai ngày 25/02/2021. Bằng tất cả tâm huyết, mình muốn đem lại cho tất cả mọi người một hệ thống luyện tập lập trình trực tuyến hiệu quả nhất, đặc biệt dành cho các bạn học sinh, sinh viên đam mê code, giải thuật! <br /> Chúc các bạn luyện tập vui vẻ!
+
             </div>
-          </li>
-        </ul>
-        <Pagination v-if="!isContest"
-                    key="page"
-                    :total="total"
-                    :page-size="limit"
-                    @on-change="getAnnouncementList">
-        </Pagination>
-      </template>
+          </Panel>
+        </Col>
+    </Row>
+    </div>
 
-      <template v-else>
-        <div v-katex v-html="announcement.content" key="content" class="content-container markdown-body"></div>
-      </template>
-    </transition-group>
-  </Panel>
-  <Row v-if="!isContest" type="flex" :gutter="10" style="margin-top: 70px;">
-    <Col :span="12">
-              <Panel shadow style="padding-top: 0px;padding-bottom: 10px;">
-                <div slot="title" style="margin-left: -10px;margin-bottom: -10px;">Bài tập mới up lên</div>
-                <ul style="margin-left: 40px;margin-bottom: 20px;">
-                  <li style="padding: 5px 0px;"  v-for="p in problemList" :key="p.id">
-                    <a class="link-style" :href="'/problem/' + p._id">{{p._id}} - {{p.title}}</a>
-                  </li>
-                </ul>
-              </Panel>
-            </Col>
-            <Col :span="12">
-              <Panel shadow style="padding: 10px;padding-bottom: 10px;">
-                <div slot="title" style="margin-left: -10px;margin-bottom: -10px;">TOP 20 PRO</div>
-                <ol style="margin-left: 40px;margin-bottom: 20px;">
-                  <li style="padding: 6px 0px;" v-for="u in dataRank" :key="u.id">
-                    <a :style="'font-weight: 600;color: ' + u.color" :href="'/user-home?username=' + u.user.username"
-                      :title=" u.title + ' ' + u.user.username">
-                    {{u.user.username}}
-                    </a> - đã giải {{u.accepted_number}} bài
-                  </li>
-                </ol>
-              </Panel>
-            </Col>
-        </Row>
-    </Col>
-  </Row>
+    <div v-if="!isContest" id="right-column">
+      <Panel shadow style="padding-top: 0px;padding-bottom: 10px;min-height: 400px;">
+        <div slot="title" style="margin-left: -10px;margin-bottom: -10px;">Top 20 PRO</div>
+        <ol style="margin-left: 40px;margin-bottom: 20px;">
+          <li v-for="u in dataRank" :key="u.id" style="margin-top:4px;">
+            <a :style="'font-weight: 600;color: ' + u.color" :href="'/user-home?username=' + u.user.username"
+               :title="u.title + ' ' + u.user.username">
+            {{u.user.username}}
+            </a> đã giải {{u.accepted_number}} bài
+          </li>
+        </ol>
+      </Panel>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -170,7 +181,7 @@
 </script>
 
 <style scoped lang="less">
-  .announcements-container {
+.announcements-container {
     margin-top: -10px;
     margin-bottom: 10px;
     li {
@@ -209,17 +220,63 @@
       }
     }
   }
-
   .content-container {
     padding: 0 20px 20px 20px;
   }
-
   .no-announcement {
     text-align: center;
     font-size: 16px;
   }changeLocale
-
   .announcement-animate-enter-active {
+    color: rgba(255, 255, 255, 0.5);
     animation: fadeIn 1s;
+  }
+  .nowWeek {
+    text-align: center;
+    padding-top: 20px;
+    font-size: 25px;
+    font-weight: 600;
+  }
+  .nowDate {
+    text-align: center;
+    padding-bottom: 10px;
+    font-size: 30px;
+    font-weight: 600;
+  }
+  .tag-btn {
+    margin-right: 10px;
+    margin-bottom: 20px;
+  }
+  .tag-btn:hover {
+    a {
+       color: #2d8cf0;
+    }
+  }
+  .link-style {
+    color:#264c67
+  }
+  .link-style:hover {
+    color: #2d8cf0;
+  }
+  .ivu-btn-text {
+    color: #57a3f3;
+  }
+  .flex-container {
+    #main {
+      flex: auto;
+      margin-right: 18px;
+      .filter {
+        margin-right: -10px;
+      }
+    }
+    #contest-menu {
+      flex: none;
+      width: 210px;
+    }
+    #right-column {
+      flex: none;
+      width: 300px;
+      max-width: 300px;
+    }
   }
 </style>
--- src/pages/oj/views/problem/Problem.vue
+++ src/pages/oj/views/problem/Problem.vue
@@ -116,7 +116,6 @@
             </v-tab>
             <v-tab title="Facebook">
               <div id="fb-root"></div>
-              <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v10.0&appId=247399839651239&autoLogAppEvents=1" nonce="xwAxfd2I"></script>
               <div class="fb-comments" :data-href="'https://codetrain.co/problem/'+ problem._id" data-width="100%" data-numposts="10"></div>
             </v-tab>
         </vue-tabs>
